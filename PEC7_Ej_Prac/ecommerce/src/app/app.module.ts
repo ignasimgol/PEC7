3957d642018dfb618743e3456b99789e5ad4b887
[Ignasi Muñoz Gol] <ignasimgol@gmail.com>
2025-01-20 09:16:10 +0400

    login and register

--- PEC7_Ej_Prac/ecommerce/src/app/app.module.ts
+++ PEC7_Ej_Prac/ecommerce/src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router'; // Importación para las rutas
 
 import { AppComponent } from './app.component';
-
 import { NavbarComponent } from './navbar/navbar.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleNewReactiveComponent } from './article-new-reactive/article-new-reactive.component';
 import { ArticleItemComponent } from './article-item/article-item.component';
 import { ImageArticlePipe } from './pipes/image-article.pipe';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+// Definición de las rutas
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'articles', component: ArticleListComponent }, // Ruta para la lista de artículos
+  { path: 'new-article', component: ArticleNewReactiveComponent }, // Ruta para el nuevo artículo reactivo
+  { path: '', redirectTo: '/login', pathMatch: 'full' } // Redirección predeterminada
+];
 
 
 @NgModule({
@@ -19,12 +30,19 @@ import { ImageArticlePipe } from './pipes/image-article.pipe';
     ArticleListComponent,
     ArticleNewReactiveComponent,
     ArticleItemComponent,
-    ImageArticlePipe
+    ImageArticlePipe,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule, FormsModule,ReactiveFormsModule,HttpClientModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)  // Configuración del RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ PEC7_Ej_Prac/ecommerce/src/app/register/register.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('Register successful', this.registerForm.value);
+    } else {
+      console.log('Form not valid');
+    }
+  }
+}
